7cf69fb90c5139e6ef42412ab40e66a2b77765ae
Kiki Pratiwi <kikipratiwi@gmail.com>
2023-05-29 20:52:55 +0700

    fix(): eslint

--- src/pages/SecondQuestion.tsx
+++ src/pages/SecondQuestion.tsx
@@ -1,16 +1,16 @@
-import { useContext } from 'react';
-import ItemProvider, { ItemContext } from '../contexts/item.context';
+import ItemProvider from '../contexts/item.context';
 import { Item } from '../components';
 
 const SecondQuestion = () => {
-    const { setName, setQty, setPrice } = useContext(ItemContext);
-
-    // example to change context value
-    const changeItemContextValue = () => {
-        setName('Brent');
-        setQty(2);
-        setPrice(10000);
-    };
+    /**
+     * Example to change context value:
+        const { setName, setQty, setPrice } = useContext(ItemContext);
+        const changeItemContextValue = () => {
+            setName('Brent');
+            setQty(2);
+            setPrice(10000);
+        };
+    */
 
     return (
         <div id="main">
